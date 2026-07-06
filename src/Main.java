4f7789400b0ea5cff38c765c433161d5e70f7c92
Thiago Pacheco <thipacheco1@gmail.com>
2026-07-06 16:57:14 -0300

    Adiciona preenchimento de arrays com scanner

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double[] atividades = new double[5];
+        Scanner scanner = new Scanner(System.in);
 
-        atividades[0] = 100.0;
-        atividades[1] = 200.0;
-        atividades[2] = 0.0;
-        atividades[3] = 150.0;
-        atividades[4] = -50.0;
+        System.out.println("Digite a quantidade de atividades:");
+        int quantidadeAtividades = scanner.nextInt();
 
-        System.out.println("----- Antes da correção -----");
-
-        for (int indice = 0; indice < atividades.length; indice++) {
-            System.out.println("Atividade na posição " + indice + ": R$ " + atividades[indice]);
+        while (quantidadeAtividades <= 0) {
+            System.out.println("Quantidade inválida. Digite novamente:");
+            quantidadeAtividades = scanner.nextInt();
         }
 
-        atividades[2] = 300.0;
-        atividades[4] = 50.0;
-
-        System.out.println("----- Depois da correção -----");
+        double[] atividades = new double[quantidadeAtividades];
 
         for (int indice = 0; indice < atividades.length; indice++) {
-            System.out.println("Atividade na posição " + indice + ": R$ " + atividades[indice]);
+            System.out.println("Digite o valor da atividade " + (indice + 1) + ":");
+            atividades[indice] = scanner.nextDouble();
         }
 
         double total = 0.0;
+        int atividadesValidas = 0;
+        int atividadesInvalidas = 0;
+
+        System.out.println("----- Atividades informadas -----");
 
         for (int indice = 0; indice < atividades.length; indice++) {
-            total = total + atividades[indice];
+            double valorAtividade = atividades[indice];
+
+            if (valorAtividade > 0) {
+                total = total + valorAtividade;
+                atividadesValidas++;
+
+                System.out.println("Atividade " + (indice + 1) + " válida: R$ " + valorAtividade);
+            } else {
+                atividadesInvalidas++;
+
+                System.out.println("Atividade " + (indice + 1) + " inválida: R$ " + valorAtividade);
+            }
         }
 
         System.out.println("----- Resumo -----");
         System.out.println("Total final: R$ " + total);
+        System.out.println("Atividades válidas: " + atividadesValidas);
+        System.out.println("Atividades inválidas: " + atividadesInvalidas);
+
+        scanner.close();
     }
 }
